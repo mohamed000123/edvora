f437e7a2d8bb4aec1f78ca3019dd557fb415a227
mohamed000123 <ma01092931340@gmail.com>
2022-02-13 06:15:50 +0200

    display products

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,14 +2,11 @@ import './home.css';
 import React , { useEffect, useState } from 'react';
 import axios from 'axios';  
 
-
-
 export const Home = ( )=>{
 const[Products,setProducts]=useState([])
 const  call = async ( )=>{
     return   await axios.get(`https://assessment-edvora.herokuapp.com`) 
 }
-
 useEffect (()=>{
 call().then( (Response) =>{
     setProducts(Response.data)
@@ -19,35 +16,8 @@ call().then( (Response) =>{
     console.log(er)
 })
 },[ ])
-
-
-
-
-//    const [cards,setCards] = useState("")
-//     { for ( let i=0 ; i<Products.length  ;  i++) (
-//          setCards(`
-//          <div className=' holder '>
-//          <img className='' src=${Products[i].image}/>
-//          <div  className='col-6'>mmmm</div>
-//         </div>   
-//          `)
-//     )
-//     document.getElementById("list-one").innerHTML = cards
-// }
-
-
-// {Products.map(
-//     (product)=>(
-//            <div className=' holder '>
-//         //         <img className='pic col-4' />
-//         //         <div  className='col-6'>mmmm</div>
-//         //   </div>      
-// ))}  
  return(
-
         <>                     
-                    
-
         <div className='container-fluid'>
               <div className='row'> 
                     <div className='menu col-md-2 col-sm-12 '>
@@ -94,45 +64,58 @@ call().then( (Response) =>{
                     </div>
                     <div className='list col-md-10 col-sm-12 '>
                                 <div className="info">
-                                  <h1 style={{color: "rgba(255, 255, 255, 0.87)"}}>Edvora</h1>
-                                  <h3 className="mt-3" style={{color: "rgba(255, 255, 255, 0.5)"}}>Products</h3>
-                                  <h3 className="mt-3 pName"  style={{color: "rgba(255, 255, 255, 1)"}}>Product Name</h3>
+                                  <h1 style={{color: "rgba(255, 255, 255, 0.87)"}} className="ml-2">Edvora</h1>
+                                  <h3 className="mt-3 ml-2" style={{color: "rgba(255, 255, 255, 0.5)"}}>Products</h3>
+                                  <h3 className="mt-3 pName ml-2"  style={{color: "rgba(255, 255, 255, 1)"}}>Microsoft inc</h3>
                                   <hr className='hr2' />
                                 </div>
                               
                                 <div className='row' id="list-one">
-                                
-                                      
-                                      <div className='holder "one mt-3  col-lg-3 col-md-4 col-sm-12" '>
+                                       {/* /////////////////// */}
+                             {/* {Products.map(
+                             (product)=>(    product.product_name == "Microsoft inc" && 
+                                      <div className='holder one mt-3  col-lg-3 col-md-4 col-sm-12 '>
                                    <div className='top row'>
-                                        <div className='col-3  img' >kkkkk </div>
-                                             <div  className='col-7 data ml-2 mt-2'>
-                                                 <h6>Product Name</h6>
-                                                 <h6>Brand Name</h6>
-                                                 <h6>$ 55.5</h6>
+                                        <div className='col-4  img' > <img src={product.image}/> </div>
+                                             <div  className='col-6 data ml-2 mt-2'>
+                                                 <h6 style={{color:"rgba(255, 255, 255, 1)"}}  className="ml-4">{product.product_name}</h6>
+                                                 <h6   style={{ color: "rgba(255, 255, 255, 0.6)"}} className="ml-4">{product.brand_name}</h6>
+                                                 <h6  style={{ color: "rgba(255, 255, 255, 1)"}} className="ml-4">{product.price}</h6>
                                              </div>
                                     </div>
                                        <div className='row '>
-                                            <div className='col-2 bottom'><h6>location</h6></div>
-                                             <div className='col-8 date'><h6 >Date:20/2/2022</h6></div>
-                                             <h5 className='location' >jjjhhh</h5>
+                                            <div className='col-2 bottom'><h6 style={{ color: "rgba(255, 255, 255, 0.6)"}}>{product.address.city}</h6></div>
+                                             <div className='col-8 date'><h6 style={{ color: "rgba(255, 255, 255, 0.6)"}} className="ml-5">Date:{product.date}</h6></div>
+                                             <h5 className='location' style={{ color: "rgba(255, 255, 255, 0.6)"}} >{product.discription}</h5>
                                        </div>
                                 </div>
-                                     
-                                    
+                             ))}   */}
                                      {/* ///////////// */}
-                                     
+
                                   </div>
                     
-                                <h3 className="mt-4 pName" style={{color:"rgba(255, 255, 255, 1)"}}>Product Name</h3>
+                                <h3 className="mt-4 pName ml-2" style={{color:"rgba(255, 255, 255, 1)"}}>Alphabet inc</h3>
                                   <hr className='hr2' />
-                                 <div className='list-two row'>
-                                      <div className="one"></div>
-                                      <div className="one"></div>
-                                      <div className="one"></div>
-                                      <div className="one"></div>
-                                 </div>
-                         
+                                 <div className='list-two  row'>
+                                 {Products.map(
+                             (product)=>(    product.product_name == "Alphabet inc" && 
+                                <div className='holder one mt-3  col-lg-3 col-md-4 col-sm-12 '>
+                                   <div className='top row'>
+                                        <div className='col-4  img' > <img src={product.image}/> </div>
+                                             <div  className='col-6 data ml-2 mt-2'>
+                                                 <h6 style={{color:"rgba(255, 255, 255, 1)"}}  className="ml-4">{product.product_name}</h6>
+                                                 <h6   style={{ color: "rgba(255, 255, 255, 0.6)"}} className="ml-4">{product.brand_name}</h6>
+                                                 <h6  style={{ color: "rgba(255, 255, 255, 1)"}} className="ml-4">{product.price}</h6>
+                                             </div>
+                                    </div>
+                                       <div className='row '>
+                                            <div className='col-2 bottom'><h6 style={{ color: "rgba(255, 255, 255, 0.6)"}}>{product.address.city}</h6></div>
+                                             <div className='col-8 date'><h6 style={{ color: "rgba(255, 255, 255, 0.6)"}} className="ml-5">Date:{product.date}</h6></div>
+                                             <h5 className='location' style={{ color: "rgba(255, 255, 255, 0.6)"}} >{product.discription}</h5>
+                                       </div>
+                                </div>
+                               ))}  
+                         </div> 
                     </div>
              </div>
        </div>
